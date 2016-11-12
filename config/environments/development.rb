9c38be36cc817622f557616e1ecca23259e8421d
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-11-12 13:09:21 -0600

    Fix time deprecation warning

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.active_record.time_zone_aware_types = [:datetime]
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
