3e8bc49b1d97fb3eb178b095a002689316f3ba0c
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-11-14 18:55:40 -0600

    Fix bug that wouldn't let me sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
