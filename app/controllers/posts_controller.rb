da68efce1723bf4657d72b85e33b9434a7cc65f4
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-11-16 10:54:51 -0600

    Added start and end time and formatted it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :date, :start_time, :end_time)
     end
 end
