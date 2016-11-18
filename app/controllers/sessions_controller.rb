d5044f3ee94808eeac3cc5f6a33d17a3d3b82b5a
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-11-18 14:01:41 -0600

    Display a flash message upon signing in successfully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       log_in(@user)
+      flash[:notice] = "You have signed in successfully!"
       redirect_to root_path
     else
       @alert = "Incorrect email or password"
