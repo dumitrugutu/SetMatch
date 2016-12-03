7cf276d8865637d2e797545b7501cdd8e8f44afb
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-12-03 15:35:38 -0600

    Updates and removes posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,12 +25,31 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path
     else
-      render 'new'
+      render('new')
     end
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to(post_path(@post))
+    else
+      render('edit')
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to(posts_path)
   end
 
   private
