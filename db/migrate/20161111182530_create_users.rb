fc8daafe69597d4b62c9168d156182c9a36d9a96
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-12-05 06:55:18 -0600

    Adds methods to edit users, and validations

--- db/migrate/20161111182530_create_users.rb
+++ db/migrate/20161111182530_create_users.rb
@@ -1,16 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string  :email, null: false
-      t.string  :fullname, null: false
-      t.string  :screen_name, null: false
-      t.string  :password_digest, null: false
-      t.text    :bio
-      t.integer :rating
-      t.integer :zipcode
-      t.integer :age
-      t.string  :gender
-      t.integer :handed
+      t.string  :email, null: false, length: 100
+      t.string  :fullname, null: false, length: 100
+      t.string  :screen_name, null: false, length: 50
+      t.string  :password_digest, null: false, length: 50
+      t.text    :bio, null: false, length: 255
+      t.integer :rating, null: false
+      t.integer :zipcode, null: false
+      t.integer :age, null: false
+      t.string  :gender, null: false
+      t.integer :handed, null: false
 
       t.timestamps
     end
